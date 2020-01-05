582d29946b32a7201a4601e3f1f80c8e8b662614
CFCode-git <435184766@qq.com>
2020-01-06 03:07:17 +0800

    build 1

---
+++ build/precache-manifest.4bae5bfbe225a416f463d4a77f51102d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7574d629a580ef62700525f495fe02da",
+    "url": "/Component-Material-button/build/index.html"
+  },
+  {
+    "revision": "e93ce00f9cda9248761c",
+    "url": "/Component-Material-button/build/static/css/main.b728697d.chunk.css"
+  },
+  {
+    "revision": "5f0a6a138f70858273f3",
+    "url": "/Component-Material-button/build/static/js/2.373b1faa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Component-Material-button/build/static/js/2.373b1faa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e93ce00f9cda9248761c",
+    "url": "/Component-Material-button/build/static/js/main.86dd5a9a.chunk.js"
+  },
+  {
+    "revision": "4a6867b80bdd3253aeb0",
+    "url": "/Component-Material-button/build/static/js/runtime-main.914edb6f.js"
+  }
+]);
---
+++ src/BoxOne.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import './BoxOne.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="BoxOne">
+                <div>Box One</div>
+            </div>
+        );
+    }
+}
+
+export default App;
